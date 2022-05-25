9c7a1c1dbe57adeda774b55baebfa324027e00c6
190714 <tejc190714@gmail.com>
2022-05-25 09:48:25 +0900

    Boot6

---
+++ src/main/java/jp/te4a/spring/boot/myapp6/BookBean.java
@@ -0,0 +1,24 @@
+package jp.te4a.spring.boot.myapp6;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class BookBean {
+	Integer id;
+	String title;
+	String writter;
+	String publisher;
+	Integer price;
+	
+	//以下@Dataにより省略
+	/*
+	public Integer getId() {
+		// TODO Auto-generated method stub
+		return null;
+	//BookRepositoryのgetid()の部分
+	}
+	 */
+	
+}
