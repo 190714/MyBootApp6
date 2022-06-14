4bee15b4f63343c70130053c3f134ca5bf75f1b6
190714 <tejc190714@gmail.com>
2022-06-14 11:28:21 +0900

    kadai11

---
+++ src/test/java/jp/te4a/spring/boot/myapp11/TestValid.java
@@ -0,0 +1,43 @@
+package jp.te4a.spring.boot.myapp11;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+public @interface TestValid {
+	String param();
+	String message() default "input other than {param}.";
+	// Classオブジェクトを得る（戻り値とする）メソッドgroups()
+	// デフォルト値は空のクラス
+	Class<?>[] groups() default {};
+	// Payloadクラスを継承したClassオブジェクトを得る
+	// （戻り値とする）メソッドpayload()、デフォルト値は空のクラス
+	Class<? extends Payload>[] payload() default{};
+	/*
+	 特に問題ない限り、バリデート用アノテーションは以下３点のみ独自記述
+	(1)インタフェース名（バリデートのアノテーション名と同じ）
+	(2)messageメソッドのdefault値（エラーメッセージ）
+	(3)param()メソッド（アノテーションのパラメタ名）
+	上記の例では、「@TestValid(param=“abc”)」のようにバリデートする
+	 */
+}
+
+/*
+
+//javadoc生成時のリンク作成
+@Documented
+//エラーチェッククラス（Validator)指定
+@Constraint(validatedBy = TestValidator.class)
+//アノテーション適用要素の指定(今回はメソッド宣言、フィールド宣言）
+@Target({ElementType.METHOD,ElementType.FIELD})
+//コンパイル後にアノテーション情報を残すか否か（今回は実行時も読み込める）
+@Retention(RetentionPolicy.RUNTIME)
+
+ */
---
+++ src/test/java/jp/te4a/spring/boot/myapp11/TestValidator.java
@@ -0,0 +1,45 @@
+package jp.te4a.spring.boot.myapp11;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class TestValidator implements ConstraintValidator<TestValid,String>{
+
+	String param;
+	@Override
+	public void initialize(TestValid nv){ param = nv.param(); }
+	@Override
+	public boolean isValid(String in,ConstraintValidatorContext cxt){
+		if(in == null){
+			return false;
+		}
+		System.out.println(in.equals(param));
+		return !in.equals(param);
+	}
+
+	@Data
+	@NoArgsConstructor
+	public class BookForm {
+		private Integer id ;
+		@NotNull
+		@Size(min = 3)
+		@TestValid(param = 
+		"abc")
+		private String title;
+		@Size(min = 3, max = 20)
+		private String writter;
+		private String publisher;
+		@Min(0)
+		private Integer price;
+	}
+}
